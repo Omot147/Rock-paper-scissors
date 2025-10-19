a3eed3eda325ae4227026caad9497615b533f026
Omot147 <177056784+Omot147@users.noreply.github.com>
2025-10-19 19:09:41 +0100

    complete functionality

--- rock-game.js
+++ rock-game.js
@@ -1,12 +1,7 @@
-
- //code gets user choice
-function getHumanChoice(){
-    let humanChoice = prompt('Enter rock, paper or scissors to defeat the monster(AI):', '');
-    return humanChoice;
-}
-console.log('human input:', getHumanChoice());
-
   // Javascript code for Rock-paper-scissors game
+ alert('WELCOME TO THE ROCK,PAPER SCISSORS GAME! Defeat the monster in 5 rounds(press the Fn + F12 key to open the console');
+ console.log('PAGE LOADED... Let the game begin!');
+ //function to get computer choice
 function getComputerChoice(){
   let randomChoice = Math.floor(Math.random() * 3 + 1);
   if (randomChoice === 1){
@@ -19,52 +14,53 @@ function getComputerChoice(){
 
 }
          let computerChoice = getComputerChoice();
-        console.log('computer output:', computerChoice);
-      
+        
 
-   function playgame(){
+        //function for entire game logic
+  function playGame(){
+    for(i = 1; i <= 5; i++){
+
+//code gets user choice
+  let humanChoice = prompt('Enter rock, paper or scissors to defeat the monster(AI):');
+function getHumanChoice(humanChoice){
+}
+ if(humanChoice === 'rock' || 'paper' || 'scissors'){
+  console.log('You:', humanChoice);
+ }else{
+  console.log('Invalid input! , please enter rock,paper or scissors');
+ }
+ console.log('computer output:', computerChoice);
        let humanScore = 0;
        let computerScore = 0;
-       
-       function playRound(humanChoice , computerChoice){
-     if(humanChoice !== computerChoice && humanChoice === 'rock' && computerChoice === 'scissors'){
-       humanScore++;
-        console.log('You win! scissors cut paper');
-     }else if(humanChoice === computerChoice){
-        console.log('This round is a tie!');
-     }else if(humanChoice !== computerChoice && computerChoice === 'rock' && humanChoice === 'scissors'){
-         computerScore++;
-        console.log('You lose! Rock crushes scissors.');
-     }else if(humanChoice !== computerChoice && computerChoice === 'paper' && humanChoice === 'rock'){
-        computerScore++;
-        console.log('You lose! Paper covers Rock.');
-     }else if(humanChoice !== computerChoice && computerChoice === 'scissors' && humanChoice === 'paper'){
-       computerScore++;
-        console.log('You lose! Scissors cuts Paper.');
-     }else{
-        console.log('Invalid input! , please enter rock,paper or scissors');
-     }
+      
+      
+       const humanSelection = getHumanChoice(humanChoice);
+       const computerSelection = getComputerChoice();
+ 
+       //function to play single round
+       function playRound(humanSelection, computerSelection){
+         if(humanChoice === 'paper' && computerChoice === 'rock' || humanChoice === 'scissors' && computerChoice === 'paper' || humanChoice === 'rock' && computerChoice === 'scissors'){
+          console.log('You beat monster!');
+          humanScore++;
+         }else if(humanChoice === computerChoice){
+          console.log('It is a tie!');
+         }else{ 
+          console.log('The monster defeated you!');
+          computerScore++
+         }
+          console.log(`Round: ${i}`);
      console.log(`USER SCORE: ${humanScore}; COMPUTER SCORE: ${computerScore}`);
-}
-      playRound(getHumanChoice(), getComputerChoice());
-
-     for(i = 1; i <= 5; i++){
-         const humanSelection = getHumanChoice();
-         const computerSelection = getComputerChoice();
+    
+        }
          playRound(humanSelection, computerSelection);
-       
-           if (i === 5) {
-      if (humanScore === computerScore) {
-        console.log(`Game's over! TIE`);
-      } else if (humanScore > computerScore) {
-        console.log(`Game's over! VICTORY`);
-      } else {
-        console.log(`Game's over! DEFEAT`);
-      }
-      console.log('Your total:',humanScore);
-      console.log('computer total:', computerScore);
+           if (i === 5) { 
+      if (humanChoice > computerChoice) {
+        console.log(`GAME's OVER!, YOU WIN...`);
+      } else{
+        console.log(`GAME OVER!, The monster has defeated you...`);
+      } 
      }  
    }
-}
-   playgame();
+  }
+   playGame();
 
